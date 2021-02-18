eeada8ef5a0a2d6edb7c5f54d42b1c2c972620cc
Elena Kot <66275938+arc-lena@users.noreply.github.com>
2021-02-18 12:49:42 +0200

    Update 1.1.16a.cpp

--- 1.1.16a.cpp
+++ 1.1.16a.cpp
@@ -6,7 +6,9 @@ using namespace std;
 float area(int x1, int y1, int x2, int y2, int x3, int y3) {
     return abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2.0);
 }
-
+bool isEqual(float x, float y){
+    return fabs(x-y) < DBL_EPSILON;
+}
 bool isInside(int x1, int y1, int x2, int y2, int x3, int y3, int x, int y) {
 
     float A = area(x1, y1, x2, y2, x3, y3);
@@ -19,7 +21,7 @@ bool isInside(int x1, int y1, int x2, int y2, int x3, int y3, int x, int y) {
 
     float A3 = area(x1, y1, x2, y2, x, y);
 
-    return (A == A1 + A2 + A3);
+    return (isEqual(A,A1 + A2 + A3));
 }
 
 
